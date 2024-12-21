c1570f88b26eb115fe4b054db64fc4fde4e6b34c
iiiMOHAMED <imohamed.abdalrahman@gmail.com>
2024-12-21 18:57:50 +0200

    Add CORS changes for OpenShift deployment

--- backend/main.go
+++ backend/main.go
@@ -91,7 +91,7 @@ func main() {
 // CORS middleware function
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:4200")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "X-Requested-With,Content-Type, Authorization")
 
